78d3323d22fe9f904f5ce47f4aad5fb8f119ebbe
Derrick Plotsky <derrickplotsky@gmail.com>
2018-02-25 17:33:06 -0800

    Add user_removed to league

--- lib/leagues/league.rb
+++ lib/leagues/league.rb
@@ -6,28 +6,54 @@ module WhatsGonnaHappen
     class League
       include Entity
 
-      def initialize
+      def self.load_from_history(events)
+        return nil if events.empty?
+        id = events[0].league_id
+        league = League.new(id)
+
+        events.each do |event|
+          league.apply_event(event)
+        end
+        league
+      end
+
+      def initialize(id)
+        @id = id
         @users = []
         on Events::Leagues::UserAdded do |event|
           apply_user_added(event)
+        end
+
+        on Events::Leagues::UserRemoved do |event|
+          apply_user_removed(event)
         end
       end
 
       def add_user(user_id)
         return if @users.any? { |u| u == user_id }
         event = Events::Leagues::UserAdded.new.tap do |e|
+          e.league_id = @id
           e.user_id = user_id
         end
         publish(event)
       end
 
-      def do_thing
-        event = Events::Event.new
+      def remove_user(user_id)
+        return unless @users.any? { |u| u == user_id }
+        event = Events::Leagues::UserRemoved.new.tap do |e|
+          e.league_id = @id
+          e.user_id = user_id
+        end
+
         publish(event)
       end
 
       def apply_user_added(event)
         @users.push(event.user_id)
+      end
+
+      def apply_user_removed(event)
+        @users.delete(event.user_id)
       end
     end
   end
--- spec/leagues/league_spec.rb
+++ spec/leagues/league_spec.rb
@@ -4,15 +4,42 @@ module WhatsGonnaHappen
   module Leagues
     RSpec.describe League do
       context 'users' do
-        it 'can add a user' do
-          league = League.new
+        let(:first_user) { 'fake' }
+        let(:league_id) { 'godzilla_heads' }
 
-          league.on Events::Leagues::UserAdded do
-            puts "got it"
+        context 'adding users' do
+          subject { League.new(league_id) }
+          it 'only generates 1 event when same user added twice' do
+            event_count = 0
+            subject.on Events::Leagues::UserAdded do |event|
+              event_count += 1
+            end
+  
+            subject.add_user(first_user)
+            subject.add_user(first_user)
+  
+            expect(event_count).to eq(1)
           end
+        end
 
-          league.add_user('fake')
-          league.add_user('fake')
+        context 'removing users' do
+          subject do
+            added_event = Events::Leagues::UserAdded.new.tap do |e|
+              e.league_id = league_id
+              e.user_id = first_user
+            end
+            league = League.load_from_history([added_event])
+            league
+          end
+          it 'generates an event when an existing user is removed' do
+            event_count = 0
+            subject.on Events::Leagues::UserRemoved do |event|
+              event_count += 1
+            end
+
+            subject.remove_user(first_user)
+            expect(event_count).to eq(1)
+          end
         end
       end
     end
