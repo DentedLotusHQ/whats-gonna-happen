93b9640a1e2615ad11e6e5d4e2915110085bc0ca
Derrick Plotsky <derrickplotsky@gmail.com>
2018-02-25 20:19:52 -0800

    Add code for setting event date

--- lib/live_events/live_event.rb
+++ lib/live_events/live_event.rb
@@ -7,6 +7,23 @@ module WhatsGonnaHappen
 
       def initialize(id)
         @id = id
+        @event_date = nil
+      end
+
+      def set_event_date(event_date)
+        return unless event_date.respond_to?(:strftime)
+        event = Events::LiveEvents::EventDateSet.new.tap do |e|
+          e.live_event_id = @id
+          e.event_date = event_date.to_s
+        end
+        publish(event)
+      end
+
+      private
+      def register
+        on Events::LiveEvents::EventDateSet do |event|
+          apply_event_date_set(event)
+        end
       end
     end
   end
--- spec/live_events/live_event_spec.rb
+++ spec/live_events/live_event_spec.rb
@@ -1,4 +1,6 @@
 require 'live_events'
+require 'events'
+require 'date'
 
 module WhatsGonnaHappen
   module LiveEvents
@@ -8,6 +10,21 @@ module WhatsGonnaHappen
         subject { LiveEvent.new(live_event_id) }
         it 'can create a live event' do
           subject
+        end
+      end
+
+      context 'dates' do
+        subject { LiveEvent.new(live_event_id) }
+        let(:event_date) { Date.new(2001, 1, 1) }
+        it 'setting the event date generates an event' do
+          event_data = nil
+
+          subject.on Events::LiveEvents::EventDateSet do |event|
+            event_data = event.event_date
+          end
+
+          subject.set_event_date(event_date)
+          expect(event_data).to eq(event_date.to_s)
         end
       end
     end
